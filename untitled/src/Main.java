a60f52588472f481d14016037298f0d9bfcb0be5
perez <raul.perez@gracia.lasalle.cat>
2024-10-11 13:00:38 +0200

    ejercio de github

---
+++ untitled/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Llegim la resposta
+        String resposta = sc.nextLine();
+
+        // Determinem la casa segons la resposta
+        String casa;
+        casa = resposta.equals("Coratge") ? "Gryffindor" :
+                resposta.equals("Coneixement") ? "Ravenclaw" :
+                        resposta.equals("Ambicio") ? "Slytherin" : "Hufflepuff";
+
+        // Mostrem la casa seleccionada
+        System.out.println(casa);
+
+
+    }
+
+}
